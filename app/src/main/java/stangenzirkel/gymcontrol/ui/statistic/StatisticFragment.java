98a4697de8e076c6201cdbdf75748f34f1846b95
Yuri Buyanov <yuri.buyanov@gmail.com>
2021-12-12 15:24:47 +0300

    progress chart added

--- app/src/main/java/stangenzirkel/gymcontrol/ui/statistic/StatisticFragment.java
+++ app/src/main/java/stangenzirkel/gymcontrol/ui/statistic/StatisticFragment.java
@@ -1,45 +1,69 @@
 package stangenzirkel.gymcontrol.ui.statistic;
 
+import com.softmoore.android.graphlib.Function;
+import com.softmoore.android.graphlib.Graph;
+import com.softmoore.android.graphlib.GraphView;
+import com.softmoore.android.graphlib.Label;
+import com.softmoore.android.graphlib.Point;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
-import stangenzirkel.gymcontrol.databinding.FragmentStatisticBinding;
+import java.util.Calendar;
+import java.util.Date;
+
+import stangenzirkel.gymcontrol.Exercise;
+import stangenzirkel.gymcontrol.ExerciseDBHelper;
+import stangenzirkel.gymcontrol.R;
 
 public class StatisticFragment extends Fragment {
+    private GraphView graphView;
 
-    private StatisticViewModel statisticViewModel;
-    private FragmentStatisticBinding binding;
+    public StatisticFragment() {
+    }
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        statisticViewModel =
-                new ViewModelProvider(this).get(StatisticViewModel.class);
-
-        binding = FragmentStatisticBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textDashboard;
-        statisticViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    public static StatisticFragment newInstance() {
+        StatisticFragment fragment = new StatisticFragment();
+        return fragment;
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_statistic, container, false);
+        graphView = view.findViewById(R.id.graph_view);
+        updateGraph();
+        return view;
+    }
+
+    public void updateGraph() {
+        Graph.Builder graphBuilder = new Graph.Builder()
+                .setWorldCoordinates(-3, 33, -20, 220)
+                .setAxes(0, 0)
+                .setBackgroundColor(0xFF2B2B2B)
+                .setAxesColor(Color.WHITE)
+                .setXTicks(new double[] {5, 10, 15, 20, 25, 30})
+                .setYTicks(new double[] {20, 40, 60, 80, 100, 120, 140, 160, 180, 200})
+                .addFunction(x -> 100, Color.GREEN);
+        int todayDate = (int) (Calendar.getInstance().getTimeInMillis() / 86400000);
+        for (Exercise exercise: ExerciseDBHelper.getInstance().getExercises()) {
+            Point[] points = new Point[30];
+            for (int i = 0; i < 30; i++) {
+                points[i] = new Point(i + 1,
+                        ExerciseDBHelper.getInstance().getExerciseProgress(exercise.id, todayDate - 30 + i, false).progress * 100 / exercise.goal);
+            }
+            graphBuilder.addLineGraph(points, Color.RED);
+        }
+        graphView.setGraph(graphBuilder.build());
     }
 }
